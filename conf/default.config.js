298fd6c5e912e70e061fee2f63ddf18c3ab4658f
张祸祸 <zhanghuohuo1996@sina.com>
2019-03-05 20:36:17 +0800

    modify

--- conf/default.config.js
+++ conf/default.config.js
@@ -5,7 +5,7 @@ module.exports = {
   port: 8080,
   theme: 'black',
   i18n: false,
-  pageTemplatePath: path.join(__dirname, 'src/pages'),
+  pageTemplatePath: 'src/containers',
   componentTemplatePath: path.join(__dirname, 'src/components'),
   jsonStoreKey: '0943a24130ba4c6054296bb7ff57e94a14df68665f817e73e5468dbbfa106a7f',
 }
